9f14909b837f3022867fe4110b9e86e760da59d7
Aleksey839482 <hromm93@gmail.com>
2024-01-09 17:20:30 +0500

    Finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿string[] array = new string[] {"Hello", "2", "world", ":-)"};
 int len_array = array.Length;
+Console.Write("Массив из 3 и менее символов: ");
+
+for(int i = 0; i < len_array; i++)
+{
+    string Str = array[i];
+    if(Str.Length <= 3)
+    {
+        Console.Write(Str + ", ");
+    }
+}
